e7c562de4bbb44f878fb98821da9d8bed326fd08
saw aung <sawaung87@gmail.com>
2025-07-30 10:28:15 +0630

    add transacation history

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "0c30f2b54281a6a1e9cd779119efd1e3",
-"assets/AssetManifest.bin.json": "fdf6d48802bd38db7a9c1839befb53df",
-"assets/AssetManifest.json": "25d2c5311b1f4e460ad14cb61dbe9db9",
+const RESOURCES = {"assets/AssetManifest.bin": "20925805925a917c5e438b5b836a2939",
+"assets/AssetManifest.bin.json": "bd7b1aadaa5242e536784412e9572d84",
+"assets/AssetManifest.json": "efe1e98b615a4eceedc1e5e484af3d24",
+"assets/assets/config/app_config.json": "7244af0ff88adcece3eec2b592b9efc7",
 "assets/assets/icons/bogolive.png": "7afd2a11575f0eb7ccd00d900f5c2074",
 "assets/assets/icons/freefire.png": "8e8eda3654ab2af33565deb999f9308d",
 "assets/assets/icons/honorking.png": "2335362c4b7570aba06ad4633bf09fee",
@@ -17,7 +18,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c30f2b54281a6a1e9cd779119efd1e3
 "assets/assets/icons/pubg.png": "389b85227ef106c5baeae51d7298b510",
 "assets/assets/icons/roblox.png": "b23ea1ef35169f7870e9601a1351d72c",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "c6417ec28ef79fe530d75f631ca2e140",
+"assets/fonts/MaterialIcons-Regular.otf": "bff662efbcc2cacabc2f459f1ca80203",
 "assets/NOTICES": "31cb1702bd96b633e6635f4d8d2eedbd",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/packages/mixpanel_flutter/assets/mixpanel.js": "48a5241deca6bca875259d2b562d7081",
@@ -33,16 +34,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c30f2b54281a6a1e9cd779119efd1e3
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "d5c70e14ace7f1d81c6327eb06ee8b1c",
+"flutter_bootstrap.js": "e6a79243a4b4aeb1e7df789719ce4e73",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a8807d49c31a255dc0846d8cb5c0f5bc",
-"/": "a8807d49c31a255dc0846d8cb5c0f5bc",
-"main.dart.js": "b53ffb4e14e3831cd19c511a9e455e46",
+"index.html": "8bae42a3cdbf11fe1de29520bbbbc2cb",
+"/": "8bae42a3cdbf11fe1de29520bbbbc2cb",
+"main.dart.js": "86da3bfefc8f0496862ba824a9f49cfc",
 "manifest.json": "a75703bfc5e77ee27e10ad93f4c28ce2",
-"version.json": "1cd8acd70cab573ccc57ff7af1e225c0"};
+"version.json": "2d3f449e39ae8d216d329ffe4a295bee"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
